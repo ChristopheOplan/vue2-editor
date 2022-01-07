bd1396bc605ec077140e1faf0cf7ffc41c1dd70e
Keven Brousseau <65184919+Keven44977@users.noreply.github.com>
2022-01-07 13:50:50 -0500

    add changes on files

--- dist/vue2-editor.core.js
+++ dist/vue2-editor.core.js
@@ -718,15 +718,14 @@
       setupCustomImageHandler: function setupCustomImageHandler() {
         var _this3 = this;
 
-        console.log("setup custom image handler");
         var toolbar = this.quill.getModule("toolbar");
         toolbar.addHandler("image", this.customImageHandler); //TODO VERIFY FILE TYPE BEFORE CONSIDERING IT AN IMAGE
 
-        document.onpaste = function (event) {
-          _this3.emitImagePasted(event);
+        // document.onpaste = function (event) {
+        //   _this3.emitImagePasted(event);
 
-          return false;
-        };
+        //   return false;
+        // };
 
         document.ondrop = function (event) {
           _this3.imageDroped(event);
@@ -740,7 +739,7 @@
           uploader.value = "";
         };
 
-        var file = $event.dataTransfer.items[0];
+        var file = $event.dataTransfer.items[0].getAsFile();
         var Editor = this.quill;
         Editor.focus();
         var range = Editor.getSelection();
